13854382d2bfd744db665172ac09ba40bcbda6c9
Jemyishai <jesse.moskowitz@gmail.com>
2020-06-17 11:08:32 -0400

    update getShops action creator

--- src/actions/getShops.js
+++ src/actions/getShops.js
@@ -1,8 +1,19 @@
-import { GET_SHOPS } from './types';
+import * as types from './types';
+import axios from 'axios';
 
-export const getShops = () => (dispatch) => {
-	dispatch({
-		type: GET_SHOPS,
-		shops: [],
-	});
+export const getShops = (shops) => {
+	const action = {
+		type: types.GET_SHOPS,
+		shops,
+	};
+	return action;
+};
+
+export const fetchShops = () => {
+	return async function thunk(dispatch) {
+		const res = await axios.get('/whatistheroutetogetallshops');
+		const allShops = res.data;
+		const actionWithData = getShops(allShops);
+		dispatch(actionWithData);
+	};
 };
